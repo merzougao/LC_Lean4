e2808cf088a7e00b0b4ff8893feac369435fe227
merzougao <merzougao@gmail.com>
2024-12-18 01:24:19 +0000

    Introducing terms and types.

--- src/Russel_System_U.lean
+++ src/Russel_System_U.lean
@@ -1,18 +1,29 @@
 
 
-inductive Universe : Type
-  | PropU : Universe
-  | TypeU : Universe
-  | KindU : Universe
+inductive Typ : Type
+  | PropU : Typ
+  | TypeU : Typ
+  | KindU : Typ
 
-inductive in_universe : Universe → Universe → Type
-  | Prop_in_Type : in_universe PropU TypeU
-  | Type_in_Kind : in_universe TypeU KindU
+inductive Term : Type
+  | propTerm : Term
+  | typeTerm : Term
 
-inductive dependent : Universe → Universe → Type
-  | PropProp : dependent PropU PropU
-  | TypeProp : dependent TypeU PropU
-  | KindProp : dependent KindU PropU
-  | TypeType : dependent TypeU TypeU
-  | KindType : dependent KindU TypeU
 
+inductive Var : Type
+  | cons : Nat → Typ → Var
+
+inductive Ctx : Type
+  | nil : Ctx
+  | cons : Var → Ctx → Ctx
+
+notation "[]" => Ctx.nil
+notation x" , "Γ => Ctx.cons x Γ
+
+def Typeof : Term → Typ :=
+  sorry
+
+inductive Typing : Ctx → Term → Typ → Type
+  | PropInType : Typing nil propTerm TypeU
+  | TypeInKind : Typing nil typeTerm KindU
+  | var : Typing Γ A s → Typing (Ctx.cons (Var.cons 0 (Typeof A)) Γ) (Var.cons 0) (Typeof A)
--- src/main2.lean
+++ src/main2.lean
@@ -1,98 +1,97 @@
 
-inductive Typ : Type 
+inductive Typ : Type
   | base : Typ
   | arrow : Typ → Typ → Typ
 
-notation A" -> "B => Typ.arrow A B 
+notation A" -> "B => Typ.arrow A B
 
 namespace Typ
 
-inductive Var : Type 
+inductive Var : Type
   | consf : Nat → Typ → Var
   | consb : Nat → Typ → Var
 
 notation x":f:"A => Var.consf x A
 notation x":b:"A => Var.consb x A
 
-inductive Term : Type 
+inductive Term : Type
   | var : Nat → Term
-  | abs : Nat → Term → Term 
-  | app : Term → Term → Term 
-  | subst : Nat → Term → Term → Term 
+  | abs : Nat → Term → Term
+  | app : Term → Term → Term
+  | subst : Nat → Term → Term → Term
 
 notation  "$"x => Term.var x
 notation "λ("x")."t => Term.abs x t
 notation t"("t'")" => Term.app t t'
 notation t"["x"//"u"]" => Term.subst x u t
 
-namespace Term 
+namespace Term
 
-inductive Ctx : Type 
+inductive Ctx : Type
   | nil : Ctx
-  | cons : Var → Ctx → Ctx 
+  | cons : Var → Ctx → Ctx
 
 notation "[]" => Ctx.nil
 notation x" , "Γ => Ctx.cons x Γ
 
-inductive notInCtx : Nat → Ctx → Type 
+inductive notInCtx : Nat → Ctx → Type
   | nil : notInCtx n []
   | cons : n ≠ x → notInCtx n Γ → notInCtx n ((x:f:A) , Γ)
 
-inductive validCtx : Ctx → Type 
+inductive validCtx : Ctx → Type
   | nil : validCtx []
   | consf : notInCtx x Γ → validCtx Γ → validCtx ((x:f:A) , Γ)
   | consb : validCtx Γ → validCtx ((x:b:A) , Γ)
 
-inductive Deduction : Ctx → Term → Typ → Type 
+inductive Deduction : Ctx → Term → Typ → Type
   | var : Deduction ((x:f:A) , []) ($ x) A
   | abs : Deduction ((x:f:A) , Γ ) t B → Deduction ((x:b:A) , Γ ) (λ(x).t) (A->B)
   | app : Deduction Γ t (A->B) → Deduction Γ t' A → Deduction Γ (t(t')) B
   | weak : validCtx ((x:f:A) , Γ) → Deduction Γ t B → Deduction ((x:f:A), Γ) t B
   | subst : Deduction ((x:f:A) , Γ) t B → Deduction Γ u A → Deduction Γ (t[x//u]) B
-  
-  
-notation Γ " ⊢ " t " : " A => Deduction Γ t A 
 
-theorem ctxSoundness : (Γ ⊢ t : A) → validCtx Γ := by 
+
+notation Γ " ⊢ " t " : " A => Deduction Γ t A
+
+theorem ctxSoundness : (Γ ⊢ t : A) → validCtx Γ := by
   intro d
-  induction d 
-  case var x B => 
-    apply validCtx.consf 
-    . apply notInCtx.nil 
+  induction d
+  case var x B =>
+    apply validCtx.consf
+    . apply notInCtx.nil
     . apply validCtx.init
   case abs  => assumption
   case app  => assumption
-  case weak  => assumption 
-  case subst => assumption 
+  case weak  => assumption
+  case subst => assumption
 
 
-inductive βreduction : Term → Term → Type 
+inductive βreduction : Term → Term → Type
   | cons : βreduction ((λ(x).t)(u)) (t[x//u])
 
-notation t₁ " ▸β " t₂ => βreduction t₁ t₂ 
+notation t₁ " ▸β " t₂ => βreduction t₁ t₂
 
 example {A B : Typ} : Σ (t : Term) , ((1:b:A) , (0:f:B->A) , []) ⊢ t : A->B->A := by
-  constructor 
+  constructor
   case fst => exact λ(1).$0
-  case snd => 
-    apply Deduction.abs 
-    apply Deduction.weak 
-    . apply validCtx.consf 
-      . apply notInCtx.cons 
+  case snd =>
+    apply Deduction.abs
+    apply Deduction.weak
+    . apply validCtx.consf
+      . apply notInCtx.cons
         . trivial
-        . apply notInCtx.nil 
-      . apply validCtx.consf 
+        . apply notInCtx.nil
+      . apply validCtx.consf
         . apply notInCtx.nil
         . apply validCtx.nil
-    . apply Deduction.var 
+    . apply Deduction.var
 
-      
-theorem reductionsPreserveTypes :  (t₁ ▸β t₂) → (Γ ⊢ t₁ : A) → (Γ ⊢ t₂ : A) := by 
-  intro d₁ d₂ 
-  induction d₂ 
+
+theorem reductionsPreserveTypes :  (t₁ ▸β t₂) → (Γ ⊢ t₁ : A) → (Γ ⊢ t₂ : A) := by
+  intro d₁ d₂
+  induction d₂
   case var  x B => contradiction
   case abs => contradiction
   case app => sorry
-  case weak =>sorry 
+  case weak =>sorry
   case subst => contradiction
-
